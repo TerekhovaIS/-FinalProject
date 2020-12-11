4121a616a5da90d30aa106a323bdf3a3e0c99f1a
TerekhovaIS <75006405+TerekhovaIS@users.noreply.github.com>
2020-12-11 10:31:56 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -15,3 +15,11 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+    
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.open_basket()
+    basket_page = BasketPage(browser, link)
+    basket_page.should_be_empty_basket_message()
