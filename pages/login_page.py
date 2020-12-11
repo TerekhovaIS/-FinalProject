b0381051876d5905d49357e512635ad12a06fe9c
TerekhovaIS <75006405+TerekhovaIS@users.noreply.github.com>
2020-12-11 10:35:07 +0300

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,14 @@
+import faker
+
 from .base_page import BasePage
 from .locators import LoginPageLocators
+
+
+def generate_email():
+    f = faker.Faker()
+    email = f.email()
+    return email
+
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,16 +17,24 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url, "Login not in current url"
-        assert True
+        assert self.browser.current_url.find("login") != -1, "URL of login page is incorrect"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*LoginPageLocators.login_form), "No autorizacion form"
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME), "Login email field is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Login password field is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON), "Login button is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*LoginPageLocators.register_form), "No register form"
-        assert True
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTRATION_EMAIL), "Registration email field is not presented"
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTRATION_PASSWORD), "Registration password field is not presented"
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTRATION_PASSWORD_REPEAT), "Registration password repeat field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_BUTTON), "Registration button is not presented"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_REPEAT).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON).click()
