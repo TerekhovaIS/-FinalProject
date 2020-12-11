9d9ef5c3185ae6eb86c4dadf6af8e302d9304c0c
TerekhovaIS <75006405+TerekhovaIS@users.noreply.github.com>
2020-12-11 10:34:36 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,30 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
-class LoginPageLocators ():
-    login_url = (By.CSS_SELECTOR, '[href="/en-gb/accounts/login/"]')
-    login_form = (By.CSS_SELECTOR, "#login_form")
-    register_form = (By.CSS_SELECTOR, "#register_form")
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
+class LoginPageLocators:
+    LOGIN_USERNAME = (By.NAME, "login-username")
+    LOGIN_PASSWORD = (By.NAME, "login-password")
+    LOGIN_BUTTON = (By.NAME, "login_submit")
+    REGISTRATION_EMAIL = (By.NAME, "registration-email")
+    REGISTRATION_PASSWORD = (By.NAME, "registration-password1")
+    REGISTRATION_PASSWORD_REPEAT = (By.NAME, "registration-password2")
+    REGISTRATION_BUTTON = (By.NAME, "registration_submit")
+
+
+class ProductPageLocators:
+    ADD_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    SUCCESS_MESSAGES = (By.CSS_SELECTOR, "#messages .alert-success")
+    INFO_MESSAGES = (By.CSS_SELECTOR, "#messages .alert-info")
+
+
+class BasketPageLocators:
+    BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner")
